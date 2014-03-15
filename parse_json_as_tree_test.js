9f85a7a5807755647b4371285fdb1d37af7b7ef3
Hackbright Student <info@hackbrightacademy.com>
2014-03-14 19:57:27 -0700

    more tree

--- parse_json_as_tree_test.js
+++ parse_json_as_tree_test.js
@@ -22,19 +22,6 @@ var s =
     };
 
 
-
-// var simple = 
-
-// console.log(s);
-// console.log(typeof(s));
-
-
-// takes in a list and returns the variable being defined
-// var_being_defined = function(ls){
-//   return ls[1];
-// };
-
-
 var BinaryTreeNode = function (value) {
   this.value = value;
   this.left = null;
@@ -60,31 +47,6 @@ var BinaryTreeNode = function (value) {
     return this.value;
   };
 };
-
-
-// :
-//     def __init__(self, value):
-//         self.left = None
-//         self.right = None
-//         self.value = value
-
-//     def get_left(self):
-//         return self.left
-
-//     def set_left(self, node):
-//         self.left = node
-
-//     def get_right(self):
-//         return self.right
-
-//     def set_right(self, node):
-//         self.right = node
-
-//     def get_value(self):
-//         return self.value
-
-//     def set_value(self, number):
-//         self.value = number
 
 test_initial = function(s){
 
@@ -119,32 +81,54 @@ test = function(list){
     var exp_symbol = exp[0];
     
     // FIX THIS. instead of exp_symbol, test(exp) should be the second arg
-    console.log(variable, "     ", exp_symbol);
+    console.log(variable, "     ", test(exp));
 
 
     // console.log("YOOOO");
     // oh...test(exp) evaluates before this entire line is printed. gotta return stuff, not print
     // well, this is more complicated than what I began with
     // console.log(variable, "     ", test(exp));
-    test(exp);
+    // test(exp);
   }
   // modify later for if first in global_env or env
   else if (first=="*"){
     var parent = new BinaryTreeNode(first);
-    // console.log(parent, parent.value);
-    var child1 = new BinaryTreeNode(list[0]);
-    var child2 = new BinaryTreeNode(list[1]);
+    // console.log(list);
 
+    // var eval_child1 = test(list[1]);
+    // var eval_child2 = test(list[2]);
+
+    var child1 = new BinaryTreeNode(list[1]);
+    var child2 = new BinaryTreeNode(list[2]);
+
+    parent.set_left(child1);
+    parent.set_right(child2);
+    // console.log(parent);
+    return parent;
 
 
     // var first_arg = list[0];
     // var second_arg = list[1];
-    
+
     // var tuple = (first_arg, second_arg);
     // console.log('this is a tuple', tuple);
     // return (first_arg, second_arg);
-    console.log(first_arg, "      ", second_arg);
+    // console.log(first_arg, "      ", second_arg);
   }
+
+
+
+
+                   // [
+                   //      "*",
+                   //      3.141592653,
+                   //      [
+                   //           "*",
+                   //           "r",
+                   //           "r"
+                   //      ]
+
+
 
 
 
