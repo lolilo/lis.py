38f839fda09f444ba7f9b32ad986c67b1f2fd314
Hackbright Student <info@hackbrightacademy.com>
2014-03-14 20:21:19 -0700

    tree

--- parse_json_as_tree_test.js
+++ parse_json_as_tree_test.js
@@ -71,73 +71,46 @@ test_initial = function(s){
 };
 
 test = function(list){
-  
-  var parent = new BinaryTreeNode(list[0]);
-  // console.log(first);
-  if (parent.value=="lambda"){
-    var lamda = parent.value;
-    var variable = new BinaryTreeNode(list[1][0]);
-    var exp = new BinaryTreeNode(list[2]);
-    // var exp_symbol = exp.value[0];
-    
 
-    parent.set_left(variable);
-    parent.set_right(exp);
-    console.log(parent);
-
-    // FIX THIS. instead of exp_symbol, test(exp) should be the second arg
-    // console.log(variable, "     ", test(exp));
-
-
-    // console.log("YOOOO");
-    // oh...test(exp) evaluates before this entire line is printed. gotta return stuff, not print
-    // well, this is more complicated than what I began with
-    // console.log(variable, "     ", test(exp));
-    // test(exp);
-  }
-  // modify later for if first in global_env or env
-  else if (parent.value=="*"){
-    // console.log(list);
-
-    console.log();
-    console.log('making children with this list', list);
-    var eval_child1 = test(list[1]);
-    console.log('inbetween');
-    var eval_child2 = test(list[2]);
-    console.log('the children are', eval_child1, eval_child2);
-
-    var child1 = new BinaryTreeNode(list[1]);
-    var child2 = new BinaryTreeNode(list[2]);
-
-    parent.set_left(child1);
-    parent.set_right(child2);
-    // console.log(parent);
-    return parent;
-  }
-
-  else {
+  if (typeof(list) != typeof([])){
     return list;
   }
 
-
-
-
-                   // [
-                   //      "*",
-                   //      3.141592653,
-                   //      [
-                   //           "*",
-                   //           "r",
-                   //           "r"
-                   //      ]
-
-
-
-
-
+  else {
+    var parent = new BinaryTreeNode(list[0]);
+    if (parent.value=="lambda"){
+      var lamda = parent.value;
+      var variable = new BinaryTreeNode(list[1][0]);
+      var exp = new BinaryTreeNode(test(list[2][0]));
+  
+      parent.set_left(variable);
+      parent.set_right(exp);
+      console.log(parent);
+    }
+    // modify later for if first in global_env or env
+    else if (parent.value=="*"){
+  
+      console.log();
+      // console.log('making children with this list', list);
+      var eval_child1 = test(list[1]);
+      // console.log('inbetween');
+      var eval_child2 = test(list[2]);
+      // console.log('the children are', eval_child1, eval_child2);
+  
+      // var child1 = new BinaryTreeNode(list[1]);
+      // var child2 = new BinaryTreeNode(list[2]);
+  
+      var child1 = new BinaryTreeNode(eval_child1);
+      var child2 = new BinaryTreeNode(eval_child2);
+  
+      parent.set_left(child1);
+      parent.set_right(child2);
+      // console.log(parent);
+      return parent;
+    }
+  }
 };
 
 test_initial(s);
 
-// console.log(Object.keys(s));
 
